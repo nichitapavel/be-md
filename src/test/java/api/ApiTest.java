ea8ef5c7f7299f474a395a67ff2dff156ba7eccd
Pavel Nichita <nichitapavel@live.com>
2018-10-23 19:21:42 +0100

    Add api testing class

--- src/test/java/api/ApiTest.java
+++ src/test/java/api/ApiTest.java
@@ -2,6 +2,7 @@ package api;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.junit.Test;
 
 import static org.hamcrest.Matchers.*;
 import static io.restassured.module.jsv.JsonSchemaValidator.*;
@@ -9,50 +10,86 @@ import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 public class ApiTest {
     String authToken;
+    String id;
 
     public ApiTest() {
         RestAssured.baseURI = "https://api.github.com/gists";
         this.authToken = System.getProperty("token");
     }
 
-    public String post_gist() {
+    @Test
+    public void postGist() {
+        System.out.println("\npostGist------------------------------GIST: " + Gists.getInstance().getID());
+        System.out.println("\npostGist------------------------------ApiTest: " + this.id);
         String jsonString = "{\"description\": \"be-md\",\"public\": false,\"files\": {\"be_md.txt\": {\"content\": \"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua\"}}}";
-        String id = null;
-        id  = RestAssured.
-        given().
-        auth().oauth2(this.authToken).
-        contentType(ContentType.JSON).
-        body(jsonString).
-        expect()
-        .log().all().
+        this.id = RestAssured.
+                given().
+                    auth().oauth2(this.authToken).
+                    contentType(ContentType.JSON).
+                    body(jsonString).
                 when().
-                post().
+                    post().
                 then().
-                statusCode(201).
-                contentType(ContentType.JSON).
-                body("id", notNullValue()).
-                        extract().
-                        path("id");
+                    statusCode(201).
+                    contentType(ContentType.JSON).
+                    body("id", notNullValue()).
+                extract().
+                    path("id");
 
-        return id;
+        Gists.getInstance().setID(this.id);
     }
 
-    public void get_gist(String id) {
+    @Test
+    public void getGist() {
+        System.out.println("\ngetGist------------------------------GIST: " + Gists.getInstance().getID());
+        System.out.println("\ngetGist------------------------------ApiTest: " + this.id);
+         RestAssured.
+            given().
+                auth().oauth2(this.authToken).
+            when().
+                get("/" + Gists.getInstance().getID()).
+            then().
+                statusCode(200).
+                contentType(ContentType.JSON);
+    }
+
+    @Test
+    public void validateGist() {
+        System.out.println("\nvalidateGist------------------------------GIST: " + Gists.getInstance().getID());
+        System.out.println("\nvalidateGist------------------------------ApiTest: " + this.id);
         RestAssured.
             given().
-            auth().oauth2(this.authToken).
-            expect()
-            .log().all().
-                    when().
-                    get("/" + id).
-                    then().
-                    statusCode(200).
-                    contentType(ContentType.JSON).
-                    assertThat().
-                        body(matchesJsonSchemaInClasspath("qa/gist-json-schema.json"));
+                auth().oauth2(this.authToken).
+            when().
+                get("/" + Gists.getInstance().getID()).
+            then().
+            contentType(ContentType.JSON).
+                body(matchesJsonSchemaInClasspath("api/gist-json-schema.json"));
     }
 
-    public void validate_gist() {
+    @Test
+    public void deleteGist() {
+        System.out.println("\ndeleteGist------------------------------GIST: " + Gists.getInstance().getID());
+        System.out.println("\ndeleteGist------------------------------ApiTest: " + this.id);
+        RestAssured.
+            given().
+                auth().oauth2(this.authToken).
+            when().
+                delete("/" + Gists.getInstance().getID()).
+            then().
+                statusCode(204);
+    }
 
+    @Test
+    public void getDeletedGist() {
+        System.out.println("\ngetDeletedGist------------------------------GIST: " + Gists.getInstance().getID());
+        System.out.println("\ngetDeletedGist------------------------------ApiTest: " + this.id);
+        RestAssured.
+            given().
+                auth().oauth2(this.authToken).
+            when().
+                get("/" + Gists.getInstance().getID()).
+            then().
+                statusCode(404);
     }
 }
